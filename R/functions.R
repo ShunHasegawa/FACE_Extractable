68e6c1dc6459e934b56c5424bf1a56ee1abb71ab
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-05 12:46:16 +1000

    type II anova is used now

--- R/functions.R
+++ R/functions.R
@@ -6,7 +6,7 @@ MdlSmpl <- function(model){
   stai <- stepAIC(mod2, trace = FALSE) #model simplification by AIC
   dr <- drop1(stai, test="Chisq") #test if removing a factor even more significantly lowers model
   model <- update(stai, method="REML")
-  ifelse(all(dr[[4]] < 0.05, na.rm=TRUE), anr <- anova(model), anr<-NA) 
+  ifelse(all(dr[[4]] < 0.05, na.rm=TRUE), anr <- Anova(model), anr<-NA)  # type II
   #dr[[4]]<0.05-->unable to remove any more factors so finlize the results by changsing the method back to REML
   return(list(step.aic = stai$anova, drop1 = dr, anova.reml = anr, model.reml = model, model.ml = stai))
 }
