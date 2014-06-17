46e5bb1f8e17c3a02e00a624b44322b5cc493107
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-17 17:00:50 +1000

    process data section is moved

--- R/Analysis.R
+++ R/Analysis.R
@@ -22,62 +22,7 @@ source("R//functions.R")
 ######################
 # Process data frame #
 ######################
-extr <- read.csv("Data//extractable.csv", 
-                 colClasses=c("ring"="factor",
-                              "plot"="factor",
-                              "time"="factor",
-                              "coverage" = "NULL"))
-# remove unnecessary rows
-extr <- droplevels(extr[complete.cases(extr), ])
-
-# rename columns
-names(extr)[c(2,6:8)] <- c("date","no", "nh", "po")
-
-
-# organise data frame
-extr <- within(extr, {
-  # format date
-  date <- as.Date(dmy(date))
-  
-  # add ID for layter analysis
-  id <- ring:plot
-  
-  # add pre and post co2, not last of pre-co2 is used as a 
-  # baseline of post-co2
-  pre <- ifelse(time %in% c(1, 2), TRUE, FALSE)
-  post <- ifelse(time != 1, TRUE, FALSE)
-  
-  # blocking
-  block <- recode(ring, 
-                  "c(1,2) = 'A'; c(3,4) = 'B'; c(5,6) = 'C'")
-})
-
-
-# save
-save(extr, file = "Output//Data/extractable.RData")
-
-##################
-# soil variables #
-##################
-load("Data/FACE_TDR_ProbeDF.RData")
-
-# subset soil
-TdrSoil <- subsetD(FACE_TDR_ProbeDF, Sample == "soil")
-
-# compute 3-month mean of soil variables for each plot
-extrSoil <- ddply(extr, .(date, ring, plot), 
-                  function(x) SoilPeriodMean(
-                    data = TdrSoil, 
-                    Start = x$date - 3 * 4 * 7, # (3 months before)
-                    End = x$date, 
-                    rings = x$ring, 
-                    plot = x$plot))
-
-# merge
-extr <- merge(extr, extrSoil, by = c("date", "ring", "plot"))
-
-# save
-save(extr, file = "Output//Data/extractable.RData")
+source("R/ProcessDF.R")
 
 
 #######################
