5cc7f79fc77aea99c28ede1c61633b22fce65376
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-17 10:41:34 +1000

    data frame is re-organised and also block is added

--- R/Analysis.R
+++ R/Analysis.R
@@ -33,16 +33,24 @@ extr <- droplevels(extr[complete.cases(extr), ])
 names(extr)[c(2,6:8)] <- c("date","no", "nh", "po")
 
 
-# format date
-extr$date <- as.Date(dmy(extr$date))
+# organise data frame
+extr <- within(extr, {
+  # format date
+  date <- as.Date(dmy(date))
+  
+  # add ID for layter analysis
+  id <- ring:plot
+  
+  # add pre and post co2, not last of pre-co2 is used as a 
+  # baseline of post-co2
+  pre <- ifelse(time %in% c(1, 2), TRUE, FALSE)
+  post <- ifelse(time != 1, TRUE, FALSE)
+  
+  # blocking
+  block <- recode(ring, 
+                  "c(1,2) = 'A'; c(3,4) = 'B'; c(5,6) = 'C'")
+})
 
-# add ID for layter analysis
-extr$id <- extr$ring:extr$plot
-
-# add pre and post co2, not last of pre-co2 is used as a 
-# baseline of post-co2
-extr$pre <- ifelse(extr$time %in% c(1, 2), TRUE, FALSE)
-extr$post <- ifelse(extr$time != 1, TRUE, FALSE)
 
 # save
 save(extr, file = "Output//Data/extractable.RData")
