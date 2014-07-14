44003116e78b470761601ea417ebe7b6f36ac500
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-14 22:06:52 +1000

    fitting different periods and compare AIC

--- R/TestDifSoilVarPeriod.R
+++ R/TestDifSoilVarPeriod.R
@@ -6,11 +6,42 @@ head(TdrSoil)
 ##################################################
 # Create mean of soil variable for ginven period #
 ##################################################
-extrSoil <- ddply(extr, .(date, ring, plot, period),
-                  # period =  number of dates to get average soil vars
-                  function(x) SoilPeriodMean(
-                    data = TdrSoil, 
-                    Start = x$date - period,
-                    End = x$date, 
-                    rings = x$ring, 
-                    plot = x$plot))
+testDF <- subsetD(extr[, c(1:12)], !pre)
+
+SoilVarPeriMean <- function(data, period){ # period =  number of dates to get average soil vars
+  ddply(data, .(date, ring, plot),
+        function(x) SoilPeriodMean(
+          data = TdrSoil, 
+          Start = x$date - period,
+          End = x$date, 
+          rings = x$ring, 
+          plot = x$plot))
+}
+  
+
+# merge
+LmrMultPeri <- function(period){
+  df <- merge(testDF, SoilVarPeriMean(testDF, period = period), by = c("date", "ring", "plot"))
+  m1 <- lmer(log(po) ~ co2 + log(Moist) + Temp_Mean + (1|block) + (1|ring) + (1|id), data = df)
+  return(m1)
+}
+
+
+LstLmrs <- llply(seq(0, 110, 1), LmrMultPeri, .progress = "text")
+names(LstLmrs) <- seq(0, 110, 1)
+aicDF <- ldply(LstLmrs, AIC)
+names(aicDF) <- c("period", "AICs")
+plot(AICs ~ period, data = aicDF)
+aicDF[which(aicDF$AICs == min(aicDF$AICs)),]
+df <- merge(testDF, SoilVarPeriMean(testDF, period = 69), by = c("date", "ring", "plot"))
+m1 <- lmer(sqrt(nh) ~ co2 * (log(Moist) + Temp_Mean) + (1|block) + (1|ring) + (1|id), data = df)
+AIC(m1)
+Anova(m1)
+m2 <- stepLmer(m1)
+Anova(m2)
+
+
+?llply
+
+names(LstLmrs2)
+LstLmrs2
