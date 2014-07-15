8084c67f9ffeb1b18b116a2fa7c9e0230280e2f5
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-15 12:16:09 +1000

    minor change

--- R/functions.R
+++ R/functions.R
@@ -183,10 +183,10 @@ RndmComp <- function(model){
 # step deletion with lmer #
 ###########################
 stepLmer <- function(model, red.rndm = FALSE, ddf = "Kenward-Roger", ...){
-  require(lmerTest)
   update(step(model, reduce.random = red.rndm, ddf = ddf,...)$model, 
          contrasts = NULL)
 }
+# require(lmerTest)
 # use "Kenward-Roger" for approximation for denominator degrees of freedom. This
 # is the same as the default DF given by Anova(model, test.statistic = "F). The
 # default of step gives me a warning message for IEM-NO3 for some reasons (not
