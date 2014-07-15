81a43119fe4587ae0ab7aaeae96cfbd98a4c0bf0
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-15 22:05:01 +1000

    function is moved to funcions.R

--- R/TestDifSoilVarPeriod.R
+++ R/TestDifSoilVarPeriod.R
@@ -17,53 +17,21 @@ TdrSoil <- subsetD(FACE_TDR_ProbeDF, Sample == "soil")
 ##################################################
 # Create mean of soil variable for ginven period #
 ##################################################
-SoilVarPeriMean <- function(data, period){ 
-  # period = number of days to back from sampling date to get average soil vars
-  df <- ddply(data, .(date, ring, plot),
-              function(x) SoilPeriodMean(
-                data = TdrSoil, 
-                Start = x$date - period,
-                End = x$date, 
-                rings = x$ring, 
-                plot = x$plot))
-  merge(data, df, by = c("date", "ring", "plot"))
-}
-  
-# Using the above function create soil variables for given period and merge with
-# data. Then run this for different periods and store all the resulted data
-# frames in a sigle list
+
+# Using the function, SoilVarPeriMean, create soil variables for given period
+# and merge with data. Then run this for different periods and store all the
+# resulted data frames in a sigle list
 
 LstDF_SoilVar <- llply(seq(0, 90, 1), 
-                       function(x) SoilVarPeriMean(data = data, period = x), 
+                       function(x) SoilVarPeriMean(data = postDF, period = x), 
                        .progress = "text")
 names(LstDF_SoilVar) <- seq(0, 90, 1)
+save(LstDF_SoilVar, file =  "Output/Data/LstDF_SoilVar.RData")
+
 
 ###############################################
 # Run lmer for each data frame and return AIC #
 ###############################################
-LmrAicComp <- function(ListDF, formula){
-  # lmer test for each data set
-  LstLmrs <- llply(ListDF, 
-                   function(x) lmer(formula, data = x),
-                   .progress = "text")
-  names(LstLmrs) <- names(ListDF)
-  
-  # plot AIC
-  aicDF <- ldply(LstLmrs, AIC)
-  names(aicDF) <- c("period", "AICs")
-  plot(AICs ~ period, data = aicDF, xlab = "N of Days back from sampling")
-  
-  
-  # lmer for the lowest aic
-  df <- ListDF[[which(aicDF$AICs == min(aicDF$AICs))]]
-  Iml <- lmer(formula, data = df)
-  Fml <- stepLmer(Iml)
-  return(list("Initial model" = Iml, "Final model" = Fml, "Data" = df))
-}
-
-
-ListDF <- LstDF_SoilVar(data = postDF, period = 30)
-
 test <- LmrAicComp(ListDF = ListDF,
                    formula = formula(log(po) ~ co2 * (log(Moist) + Temp_Mean) + 
                      (1|block) + (1|ring) + (1|id)))
