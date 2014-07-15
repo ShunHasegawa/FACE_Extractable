c1498e6f9c9e32f69df37c26b2c70123ab7b43a3
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-07-15 22:09:37 +1000

    moved to Stats.R

--- R/Stats.R
+++ R/Stats.R
@@ -1,6 +1,40 @@
 # parcent change
 pchDF <- ddply(extr, .(ring, plot, co2, block, id), PerChange)
 
+
+
+
+#########################################
+# Process soil variable data for ANCOVA #
+#########################################
+
+# Soil variables (i.g. moisture and temperature) will be used as covariates. I 
+# need to deternmine how many days to go back from the sampling dates in order 
+# to calculate their means for given period. The number of days to be used will
+# be determined by AIC valuse for models with different periods used to obtain
+# soil variable averages.
+
+# TDR soil data
+load("Data/FACE_TDR_ProbeDF.RData")
+
+# subset soil
+TdrSoil <- subsetD(FACE_TDR_ProbeDF, Sample == "soil")
+
+##################################################
+# Create mean of soil variable for ginven period #
+##################################################
+
+# Using the function, SoilVarPeriMean, create soil variables for given period
+# and merge with data. Then run this for different periods and store all the
+# resulted data frames in a sigle list
+
+LstDF_SoilVar <- llply(seq(0, 90, 1), 
+                       function(x) SoilVarPeriMean(data = postDF, period = x, SoilData = TdrSoil), 
+                       .progress = "text")
+names(LstDF_SoilVar) <- seq(0, 90, 1)
+save(LstDF_SoilVar, file =  "Output/Data/LstDF_SoilVar.RData")
+load("Output/Data/LstDF_SoilVar.RData")
+
 ###########
 # Nitrate #
 ###########
