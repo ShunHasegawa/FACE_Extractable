01f3406b2d539d833bfdbf25650c80bb737ab136
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-10-06 01:06:08 +0100

    no interaction for p any more

--- R/Stats.R
+++ R/Stats.R
@@ -81,5 +81,5 @@ save(Stat_CO2Time, file = "Output//Data/FACE_extractable_CO2xTime_Stats.RData")
 ########################
 ## Result of contrast ##
 ########################
-ContrastDF <- rbind(FACE_Extr_PostCO2_NH_CntrstDf, FACE_Extr_PostCO2_PO_CntrstDf)
+ContrastDF <- rbind(FACE_Extr_PostCO2_NH_CntrstDf)
 save(ContrastDF, file = "Output//Data/FACE_Extractable_ContrastDF.RData")
